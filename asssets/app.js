7a4d6e0899ccf4266198c88330864234561b59b0
David Baker <daviiddamiianbaker@gmail.com>
2022-02-02 19:26:55 -0500

    wip

---
+++ asssets/app.js
@@ -0,0 +1,17 @@
+let itemsToGuess = 
+[
+    'netflix',
+    'hulu',
+    'paramount',
+    'amc',
+    'shudder',
+    'acorn tv',
+    'sundance now',
+    'youtube',
+    'amazon prime video',
+    'allblk tv'
+]
+
+for (let i = 0; i < itemsToGuess.length; i++) {
+    console.log(itemsToGuess);
+}
