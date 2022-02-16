6d592e183cb8f7dec1417898559dc1fbc1f03226
db <34483246+DavidDBaker@users.noreply.github.com>
2022-02-15 20:49:09 -0500

    wip

--- assets/app.js
+++ assets/app.js
@@ -16,31 +16,14 @@ let startGame = false;
 let timer = 60000;
 
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-window.onload = function () {
-    var fiveMinutes = 60000,
-        display = document.querySelector('#timer');
-    startTimer(fiveMinutes, display);
-};
+ 
+itemsToGuess.forEach(el => {
+    console.log(el[2])
+})
 
 
 // begin game - click any button to start
+
 
 // document.addEventListener('keyup', function(el) {
 
