ab190dd09e92bd98b92bd1de20ad92c6068d70f7
db <34483246+DavidDBaker@users.noreply.github.com>
2022-02-15 21:10:22 -0500

    generating letter btn

--- assets/app.js
+++ assets/app.js
@@ -19,7 +19,24 @@ let guessed = [];
 
 function randomWord() {
     answers = streamingServices[Math.floor(Math.random() * streamingServices.length)]
-    console.log(answers)
 }
 
+function generateButtons() {
+    let buttonsHtml = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => 
+        `
+        <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `
+        onClick="handleGuess('` + letter + `')"
+        >
+        ` + letter + `
+        </button>
+        `).join('');
+
+        document.getElementById('keyboard').innerHTML = buttonsHtml;
+}
+
+
+
 randomWord();
+generateButtons()
